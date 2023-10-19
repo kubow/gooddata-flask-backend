7bba0f606d1961fc3ac6f1866a011bbed75b6402
jan <xpanj19@gmail.com>
2023-10-19 13:01:23 +0200

    add nested level hieararchy and fix structure

--- common.py
+++ common.py
@@ -18,6 +18,7 @@ class LoadGoodDataSdk:
         self.groups = self._sdk.catalog_user.list_user_groups()
         self.datasources = self._sdk.catalog_data_source.list_data_sources()
         self.workspaces = self._sdk.catalog_workspace.list_workspaces()
+        self.declarative_workspaces = self._sdk.catalog_workspace.get_declarative_workspaces()
 
     def organization(self):
         print(f"\nCurrent organization info:")  # ORGANIZATION INFO
@@ -173,28 +174,36 @@ def wipe_workspaces_and_permissions(
 
 
 def visualize_workspace_hierarchy(sdk: classmethod):
-    hierarchy = {}
+    data = {}
 
     tree = Tree()
     tree.create_node("GoodData", "root")
     for workspace in sdk.catalog_workspace.list_workspaces():
         parent_id = workspace.parent_id if workspace.parent_id else "root"
         #print(workspace.id, workspace.name, workspace.parent_id)
-        hierarchy[workspace.id] = {
+        data[workspace.id] = {
             'name': workspace.name,
             'parent_id': workspace.parent_id
         }
-        json_data = json.dumps(hierarchy, indent=4)
-        if tree.get_node(workspace.id):
-            continue  # we already established the node
-        elif tree.get_node(parent_id):
-            tree.create_node(workspace.name, workspace.id, parent=parent_id)
-        else:
-            temp_root = sdk.catalog_workspace.get_workspace(parent_id)
-            temp_parent_id = temp_root.parent_id if temp_root.parent_id else "root"
-            tree.create_node(temp_root.name, temp_root.id, parent=temp_parent_id)
-            tree.create_node(workspace.name, workspace.id, parent=parent_id)
-    tree.show(line_type="ascii-em")
+
+        def create_workspace_structure(data, parent_id=None):
+            workspace_list = []
+
+            for key, value in data.items():
+                if value["parent_id"] == parent_id:
+                    workspace = {
+                        "id": key,
+                        "name": value["name"],
+                        "parent_id": parent_id,
+                        "children": create_workspace_structure(data, key)
+                    }
+                    workspace_list.append(workspace)
+
+            return workspace_list
+
+        workspace_structure = create_workspace_structure(data)
+        json_data = json.dumps(workspace_structure, indent=4)
+
     return json_data
 
 
@@ -208,11 +217,50 @@ def visualize_workspace_hierarchy(sdk: classmethod):
 #     }
 # }
 
+# data = {
+#     "ws hierarchy": {
+#         "ws_id1": {
+#             "name": "Workspace Name 1",
+#             "wdf_id": "WDF Client ID 1",
+#             "parent_id": None
+#         },
+#         "ws_id2": {
+#             "name": "Workspace Name 2",
+#             "wdf_id": "WDF Client ID 2",
+#             "parent_id": "ws_id1"
+#         },
+#         "ws_id3": {
+#             "name": "Workspace Name 3",
+#             "wdf_id": "WDF Client ID 3",
+#             "parent_id": None
+#         }
+#     }
+# }
+
+# def create_workspace(ws_id, name):
+#     # Call the function to create a workspace
+#     sdk.catalog_workspace.create_or_update(
+#         CatalogWorkspace(
+#             workspace_id=ws_id,
+#             name=name
+#         )
+#     )
+
 
 if __name__ == "__main__":
     # host, token, sdk = init_gd()
     gooddata = LoadGoodDataSdk()
     for user in gooddata.users:
         print(f"user {user.id} with relations {user.relationships}")
+
+    #VIEW func
     json_data = visualize_workspace_hierarchy(gooddata._sdk)
     print(json_data)
+
+    #print(gooddata.declarative_workspaces)
+
+    # #CREATE func
+    # # Iterate through the data and call the function for each key where parent_id is None
+    # for ws_id, workspace_data in data.get("ws hierarchy", {}).items():
+    #     if workspace_data.get("parent_id") is None:
+    #         create_workspace(ws_id, workspace_data.get("name"))
